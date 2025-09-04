86b8f56a7bdf25f47d8bff80611fab172cd9b7d2
username <wecp-dummy@gmail.com>
2025-09-04 13:14:26 +0000

    Day9

--- backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
@@ -1,5 +1,22 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface SupplierRepository {
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.service.impl.SupplierServiceImplJdbc;
+
+public interface SupplierRepository extends JpaRepository<Supplier,Integer>{
+    
+    
+
+    public Supplier deleteBySupplierId(@Param("supplierId") int supplierId);
+
+    public Supplier findBySupplierId(@Param("supplierId") int supplierId);
+
+    public Object findByEmail(String email);
+
+    public Object findByUsername(String username);
+
 }
